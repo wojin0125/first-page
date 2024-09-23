58bd1bf391da906a5cee093898332768bc4ee669
wojin0125 <22146@dongdaemun.ms.kr>
2024-09-23 16:12:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,17 +19,17 @@ st.caption('팔로우부탁해요')
 #코드 표시
 sample_code = '''
 def function():
-    print('hello, world')
+    print('hello, minjae')
 '''
 st.code(sample_code, language="python")
 
 #일반 텍스트
-st.text('일반적인 텍스트를 입력해 보았습니다.')
+st.text('문의는 DM으로 보탁드립니다')
 
 # 마크다운 문법 지원
-st.markdown('streamlit은 **마크다운 문법을 지원**합니다.')
+st.markdown('민재에게 **학교에대한 건의사항**을 보내주세요')
 #컬러코드: blue, green, orange, red, violet
-st.markdown("텍스트의 색상을 :green[초록색]으로, 그리고 **:blue[파란색]** 볼트체로 설정할 수 있습니다.")
+st.markdown("민재가 :green[DM을 확인] 하면 **:blue[서민재]** 학교를 여러분의 의견을 반영하여 바꾸어드립니다.")
 st.markdown(" :green[$\sqrt{x^2+y^2}=1$] 와 같이 latex 문법의 수식 표현도 가능합니다 :pencil:")
 
 #Latex 수식 지원
