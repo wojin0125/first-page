6d2fd5927689c57aa4471b90342fd6717150f359
wojin0125 <22146@dongdaemun.ms.kr>
2024-09-23 15:56:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ st.text('일반적인 텍스트를 입력해 보았습니다.')
 st.markdowm('streamlit은 **마크다운 문법을 지원**합니다.')
 #컬러코드: blue, green, orange, red, violet
 st.markdown("텍스트의 색상을 :green1[초록색]으로, 그리고 **:blue[파란색]** 볼트체로 설정할 수 있습니다.")
-st.markdown(" :green[$/sprt{x^2+y^2}=1$] 와 같이 latex 문법의 수식 표현도 가능합니다 :pencil:")
+st.markdown(" :green[$\sqrt{x^2+y^2}=1$] 와 같이 latex 문법의 수식 표현도 가능합니다 :pencil:")
 
 #Latex 수식 지원
-st.latex(r'/sprt{x^2+y^2}=1')
+st.latex(r'\sqrt{x^2+y^2}=1')
