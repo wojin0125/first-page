0e5f9281c85d1de92bbf4bc38fa5e230b8bb721f
wojin0125 <22146@dongdaemun.ms.kr>
2024-09-23 16:13:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ st.header(':sparkles:동대문중 학생회장 서민재:sparkles:')
 st.subheader('09_minjae_seo')
 
 #캡션 적용
-st.caption('팔로우부탁해요')
+st.caption('인스타 팔로우부탁해요')
 
 #코드 표시
 sample_code = '''
@@ -29,7 +29,7 @@ st.text('문의는 DM으로 보탁드립니다')
 # 마크다운 문법 지원
 st.markdown('민재에게 **학교에대한 건의사항**을 보내주세요')
 #컬러코드: blue, green, orange, red, violet
-st.markdown("민재가 :green[DM을 확인] 하면 **:blue[서민재]** 학교를 여러분의 의견을 반영하여 바꾸어드립니다.")
+st.markdown("민재가 :green[DM을 확인] 하면 **:blue[서민재]**가 학교를 여러분의 의견을 반영하여 바꾸어드립니다.")
 st.markdown(" :green[$\sqrt{x^2+y^2}=1$] 와 같이 latex 문법의 수식 표현도 가능합니다 :pencil:")
 
 #Latex 수식 지원
