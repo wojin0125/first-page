9da04144053e69bc9c8a948d917a1aaf0b7c039b
wojin0125 <22146@dongdaemun.ms.kr>
2024-09-23 15:38:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import streamlit as st
-st.title("My first web service!")
-name=st.text_input("이름을 입력해주세요!")
-menu=st.selectbox("좋아하는 음식을 선택해주세요!",['치킨','짜장면','피자'])
-if st.button("인사말 생성하기"):
-    st.write(name+"님! 당신이 좋아하는 음식은 "+menu+"이군요! 저도 좋아요!")
+
+# 타이틀 적용 예시
+st.title('서민재를 소개합니다.')
+
+# 특수 이모티콘 삽입 예시
+#emoji: httpsL://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
+st.title('스마일 :sunglasses')
+
+#Header 적용
+st.header('헤더는 섹션의 제목 :sparkles:')
+
+#Subheader 적용
+st.subheader('subheader는 섹션의 부제목')
+
+#캡션 적용
+st.caption('캡션은 짧은 설명을 추가하는 것!')
+
+#코드 표시
+sample_code = '''
+def function():
+    print('hello, world)
+'''
+st.code(sample_code, langueage="python")
+
+#일반 텍스트
+st.text('일반적인 텍스트를 입력해 보았습니다.')
