a486f1f5069d81f6c302ed36c363b4a9c631190f
wojin0125 <22146@dongdaemun.ms.kr>
2024-09-23 15:48:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 # 타이틀 적용 예시
-st.title('서민재를 소개합니다.')
+st.title('학생회장 서민재 팬카페.')
 
 # 특수 이모티콘 삽입 예시
 #emoji: httpsL://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
-st.title('스마일 :sunglasses')
+st.title('★')
 
 #Header 적용
 st.header('헤더는 섹션의 제목 :sparkles:')
