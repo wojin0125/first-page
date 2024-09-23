6c3876ae81ecba165e2f81f9202ae81bc0710359
wojin0125 <22146@dongdaemun.ms.kr>
2024-09-23 15:55:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # 타이틀 적용 예시
-st.title('학생회장 서민재 팬카페.')
+st.title('학생회장 서민재 팬카페')
 
 # 특수 이모티콘 삽입 예시
 #emoji: httpsL://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
@@ -25,3 +25,12 @@ st.code(sample_code, language="python")
 
 #일반 텍스트
 st.text('일반적인 텍스트를 입력해 보았습니다.')
+
+# 마크다운 문법 지원
+st.markdowm('streamlit은 **마크다운 문법을 지원**합니다.')
+#컬러코드: blue, green, orange, red, violet
+st.markdown("텍스트의 색상을 :green1[초록색]으로, 그리고 **:blue[파란색]** 볼트체로 설정할 수 있습니다.")
+st.markdown(" :green[$/sprt{x^2+y^2}=1$] 와 같이 latex 문법의 수식 표현도 가능합니다 :pencil:")
+
+#Latex 수식 지원
+st.latex(r'/sprt{x^2+y^2}=1')
