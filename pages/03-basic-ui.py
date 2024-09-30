5877eaf9636b169247cd15e6e0ec851117cb7aad
wojin0125 <22146@dongdaemun.ms.kr>
2024-09-30 15:53:17 +0900

    Update 03-basic-ui.py

--- pages/03-basic-ui.py
+++ pages/03-basic-ui.py
@@ -31,10 +31,3 @@ if agree:
 mbti = st.radio(
     '당신의 MBTI는 무엇입니까?',
     (ISTJ, 'ENFP', '선택지 없음'))
-
-if mbti == 'ISTJ':
-    st.write('당신은 :blue[현실주의자] 이시네요')
-elif mbti == 'ENFP':
-    st.write('당신은 :green[활동가] 이시네요')
-else:
-    st.write("당신에 대해 :red[알고 싶어요]:grey_exclametion:")
