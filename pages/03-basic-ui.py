91030817dbb852a8f9e0de809a5466719dfa538d
wojin0125 <22146@dongdaemun.ms.kr>
2024-09-30 15:20:07 +0900

    Create 03-basic-ui.py

---
+++ pages/03-basic-ui.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+from datetime import dadetime as dt
+import datetime
+
+#버튼 클릭
+button = st.button('버튼을 눌러보세요')
+if button:
+    st.wrtie(':blue[버튼]이 눌렸습니다 :sparkles:')
