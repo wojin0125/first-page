f7421381e21112e621e43c3bf5ebc84d89431ec2
wojin0125 <22146@dongdaemun.ms.kr>
2024-09-30 15:31:47 +0900

    Update 03-basic-ui.py

--- pages/03-basic-ui.py
+++ pages/03-basic-ui.py
@@ -7,3 +7,18 @@ import datetime
 button = st.button('버튼을 눌러보세요')
 if button:
     st.write(':blue[버튼]이 눌렸습니다 :sparkles:')
+
+# 파일 다운로드 버튼
+# 샘플 데이터 생성
+# Dataframe이란, pandas라이브러리에서 제공하는 2차원 데잍터 구조(엑셀과 유사)
+dataframe = pf.DataFrame({
+    'first column': ['kor','eng','math','science'],
+    'second column': [10, 20, 30, 40]
+})
+
+#다운로드 버튼 연결
+st.download_button(
+    label='CSV로 성적표 다운로드',
+    date=dateframe.to_csv(),      # dateframe을 csv형태로 변환
+    file_name='sample.csv',
+    mime='text/csv'               #데이터 유형
