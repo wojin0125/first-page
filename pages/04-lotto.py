4f0a1527da5f6f9d44053481ff3feff38b75cb08
wojin0125 <22146@dongdaemun.ms.kr>
2024-10-07 15:23:30 +0900

    Update 04-lotto.py

--- pages/04-lotto.py
+++ pages/04-lotto.py
@@ -15,3 +15,10 @@ def generate_lotto():
   lotto = list(lotto)
   lotto.sort()
   return lotto
+
+button = st.button('로또를 생성해 주세요!')
+
+if button:
+  for i in range(1, 6):
+      st.subheader(f'{i}. 행운의 번호: :green[{generate_lotto()}]')
+  st.write(f"생성된 시각: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}")
